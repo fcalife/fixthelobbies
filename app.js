aa5ee8f6ab5d2a8cc525cd10a75b4e20cca08c55
Anton Winkler <aw93mita@studserv.uni-leipzig.de>
2021-03-30 14:00:05 +0200

    added language support

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 function ExpandOtherIssues() {
 	const issueContainer = document.getElementById("OtherIssuesTitle");
 	const issueBody = document.getElementById("OtherIssuesBody");
@@ -14,4 +13,27 @@ function ExpandOtherIssues() {
 		issueContainer.classList.remove("open");
 		issueBody.style.height = "0px";
 	}
+}
+
+function ChangeLanguage(target) {
+	console.log("TEST");
+	const curId = target.getAttribute("id");
+	const container = document.getElementById("LanguageContainer");
+	for (const child of container.children) {
+		if (child.id == curId) {
+			child.classList.add("selected");
+		} else {
+			child.classList.remove("selected");
+		}
+	}
+	
+	let targetPath = "";
+	if (curId === "ru") {
+		targetPath = "_russian";
+	} else if (curId === "ch") {
+		targetPath = "_chinese";
+	}
+
+	
+	window.location.href = "index" + targetPath + ".html";
 }
